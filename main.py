7f882443f623ed840f9f95e583ce2a34a63ce4f9
Yuliyakulikova <uskulikova@yandex.ru>
2023-07-07 17:02:16 +0500

    first

---
+++ main.py
@@ -0,0 +1,71 @@
+import types
+
+
+# task 1
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+
+    def __iter__(self):
+        self.results = []
+        self.list_1 = iter(self.list_of_list)
+        self.counter = -1
+        return self
+
+    def __next__(self):
+        self.counter += 1
+        if len(self.results) == self.counter:
+            self.counter = 0
+            self.results = next(self.list_1)
+        item = self.results[self.counter]
+        return item
+
+
+def test_1():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
+
+
+# task 2
+def flat_generator(list_of_lists):
+    for counter in range(len(list_of_lists)):
+        for i in list_of_lists[counter]:
+            yield i
+
+
+def test_2():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_2()
